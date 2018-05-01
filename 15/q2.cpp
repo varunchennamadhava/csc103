9b617410a261b328929bef8b22ab26dd882b734e
csc103 student <me@citymail.ccny.cuny.edu>
2018-05-01 18:06:46 -0400

    15

---
+++ 15/q2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int power(int x, int n)
+{
+  if (n==1)
+  {
+    return x;
+  }
+  return power(x, n-1) * x;
+}
+
+int main()
+{
+  int y,w;
+  cout << "Enter x: ";
+  cin >> y;
+  cout << endl;
+  cout << "Enter n: ";
+  cin >> w;
+  cout << power(y,w) << endl;
+}
